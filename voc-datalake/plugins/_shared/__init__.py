3eed244b7246a6c39b7311fa29a4335fdad28bf9
Stefano Vozza <svozza@amazon.com>
2026-02-05 11:30:21 +0000

    remove unused webhook auth module (#52)

--- voc-datalake/plugins/_shared/__init__.py
+++ voc-datalake/plugins/_shared/__init__.py
@@ -6,7 +6,6 @@ from .base_ingestor import BaseIngestor
 from .base_webhook import BaseWebhook
 from .audit import emit_audit_event, AuditAction
 from .circuit_breaker import CircuitBreaker
-from .webhook_auth import require_webhook_signature, WebhookAuthError
 
 __all__ = [
     "BaseIngestor",
@@ -14,6 +13,4 @@ __all__ = [
     "emit_audit_event",
     "AuditAction",
     "CircuitBreaker",
-    "require_webhook_signature",
-    "WebhookAuthError",
 ]
